a7f859f3ec74cb2b46aadec61a13bb692c8808ad
aishapeng <aishaaishapeng@gmail.com>
2020-11-22 23:42:17 +0800

    integrate recycler view in fragment

--- app/src/main/java/com/example/mykid/RecyclerViewFragment.java
+++ app/src/main/java/com/example/mykid/RecyclerViewFragment.java
@@ -1,17 +1,25 @@
 package com.example.mykid;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 
 public class RecyclerViewFragment extends Fragment {
 
-
+    private ActivityViewModel activityViewModel;
+    private RecyclerView recyclerView;
 
     public RecyclerViewFragment() {
         // Required empty public constructor
@@ -28,6 +36,8 @@ public class RecyclerViewFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         if (getArguments() != null) {
 
         }
@@ -36,6 +46,18 @@ public class RecyclerViewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        recyclerView = container.findViewById(R.id.recyclerview);
+        final ActivityListAdapter adapter = new ActivityListAdapter(getContext());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        activityViewModel = new ViewModelProvider(this).get(ActivityViewModel.class);
+        activityViewModel.getAllActivity().observe(getViewLifecycleOwner(), new Observer<List<Activity>>() {
+            @Override
+            public void onChanged(List<Activity> activities) {
+
+            }
+        });
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_recycler_view, container, false);
     }
