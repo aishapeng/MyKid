cc972681686ffcc26b4ed4d2d3bb89095baf53a1
yuxuankok <kokyuxuan2000.ky@gmail.com>
2020-11-23 16:56:18 +0800

    kyx

--- app/src/main/java/com/example/mykid/AddReportFrag.java
+++ app/src/main/java/com/example/mykid/AddReportFrag.java
@@ -21,35 +21,6 @@ public class AddReportFrag extends Fragment implements View.OnClickListener {
     private TextView dateInputTxtView;
     private TextView timeInputTxtView;
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    private String mParam1;
-    private String mParam2;
-
-    public AddReportFrag() {
-        // Required empty public constructor
-    }
-    
-    public static AddReportFrag newInstance(String param1, String param2) {
-        AddReportFrag fragment = new AddReportFrag();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
